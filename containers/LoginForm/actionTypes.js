ff93a6a58a0d89ef4ccebcdc6fe8f527b3f11827
SOFTWARE-GROUP\Mariela.Kasovska <mariela.kasovska@softwaregroup-bg.com>
2016-11-30 17:12:15 +0200

    feat: UIS-422:  Setup initial component structure

---
+++ containers/LoginForm/actionTypes.js
@@ -0,0 +1,3 @@
+export const actionList = {
+    LOGIN: Symbol('LOGIN')
+};
--- initApp.js
+++ initApp.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from 'react';
 import { Route } from 'react-router';
-import { Login, Gate, Master, Layout } from './pages';
+import { Gate, Master, Layout } from './pages';
+import LoginForm from './containers/LoginForm';
 
 const App = ({routes, extLayout, extMaster}) => {
     return (
         <Route>
-            <Route path='/login' component={Login} />
+            <Route path='/login' component={LoginForm} />
             <Route component={Gate}>
                 <Route component={extMaster || Master}>
                     <Route component={extLayout || Layout}>
