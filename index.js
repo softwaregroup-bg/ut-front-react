f37c49adcce79bd4663147be98a279a02c16079f
elin.angelov <elin.angelov@softwaregroup-bg.com>
2016-11-30 16:25:57 +0200

    fix: UIS-461

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var merge = require('lodash.merge');
 
 module.exports = {
     start: function() {
@@ -14,41 +13,6 @@ module.exports = {
                     index: true,
                     lookupCompressed: true
                 }
-            }
-        }, {
-            method: 'GET',
-            path: '/',
-            config: {auth: false},
-            handler: (request, reply) => {
-                this.config['utfront.pack'](merge({}, this.config, {minifyJS: false, bundlingEnabled: false}))
-                    .then(function(pack) {
-                        reply(`
-                            <!DOCTYPE html>
-                            <html lang="en">
-                                <head>
-                                    <!-- Required meta tags always come first -->
-                                    <meta charset="utf-8">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                    <meta http-equiv="x-ua-compatible" content="ie=edge">
-                                    <script type="text/javascript">
-                                        global=window;
-                                    </script>
-                                    <title>UnderTree</title>
-
-                                    <!-- Bootstrap CSS -->
-                                    <link rel="stylesheet" href="/static/assets/css/bootstrap.min.css">
-                                    ${pack.head}
-                                </head>
-                                <body class="ut5">
-                                    ${pack.body}
-                                </body>
-                            </html>
-                        `);
-                        return;
-                    })
-                    .catch((e) => {
-                        throw new Error(e);
-                    });
             }
         }]);
     }
