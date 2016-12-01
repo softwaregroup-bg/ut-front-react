da2c87119819d287d55dc1013bef374e7c5f2639
SOFTWARE-GROUP\Mariela.Kasovska <mariela.kasovska@softwaregroup-bg.com>
2016-12-01 10:21:51 +0200

    feat:UIS-422

--- containers/LoginForm/actionTypes.js
+++ containers/LoginForm/actionTypes.js
@@ -1,3 +1,6 @@
 export const actionList = {
-    LOGIN: Symbol('LOGIN')
+    LOGIN: Symbol('LOGIN'),
+    LOGOUT: Symbol('LOGOUT'),
+    CHECK_COOKIE: Symbol('CHECK_COOKIE'),
+    SET_LOGIN_DATA: Symbol('SET_LOGIN_DATA')
 };
--- containers/LoginForm/reducer.js
+++ containers/LoginForm/reducer.js
@@ -1,1 +1,1 @@
-
+import { LOGIN, LOGOUT, CHECK_COOKIE, SET_LOGIN_DATA } from './actionTypes';
