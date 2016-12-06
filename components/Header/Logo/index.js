a4fff9619b1cb3de6f271d56fb38dbecc2cf7639
Anton Bozhinov <anton.bozhinov@softwaregroup-bg.com>
2016-12-06 16:37:56 +0200

    fix: UIS-487 fix lint

--- components/Header/Logo/index.js
+++ components/Header/Logo/index.js
@@ -25,6 +25,5 @@ Logo.propTypes = {
 Logo.contextTypes = {
     implementationStyle: PropTypes.object,
     mainUrl: PropTypes.string,
-    implementationStyle: PropTypes.object,
     router: PropTypes.object.isRequired
 };
