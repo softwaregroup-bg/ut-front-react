26012fe47e09a31a890fea187153c82a6650d0af
Emilia Pavlova <emiliya.pavlova@softwaregroup-bg.com>
2016-12-06 17:02:12 +0200

    feat: UIS-451: change all images in standard implementation

--- components/Input/CheckPlus.js
+++ components/Input/CheckPlus.js
@@ -1,12 +1,12 @@
 import React, { PropTypes } from 'react';
-import Image from '../Image';
 import style from './style.css';
-import plusImage from './images/addPink.png';
-import checkboxImage from './images/checkedPink.png';
+import classnames from 'classnames';
 
 const CheckPlus = (props) => (
     <span className={style.checkBoxWrapper + ' ' + style.checkPlusPointer}>
-        {props.checked ? <Image src={checkboxImage} className={props.className} style={props.style} onClick={props.onClick} /> : <Image src={plusImage} className={props.className} style={props.style} onClick={props.onClick} />}
+        {props.checked
+            ? <div className={classnames(props.className, style.checked)} style={props.style} onClick={props.onClick} />
+            : <div className={classnames(props.className, style.unchecked)} style={props.style} onClick={props.onClick} />}
         <label><span /></label>
         <p>{props.label}</p>
     </span>
