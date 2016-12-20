163a96bb61e4ee0f83e21b3fb05e24afc28d12d2
elin.angelov <elin.angelov@softwaregroup-bg.com>
2016-12-20 09:54:50 +0200

    feat: volumn hide/show

--- components/SimpleGrid/Header/Field.js
+++ components/SimpleGrid/Header/Field.js
@@ -1,12 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
 import { propTypeField } from '../common';
+import Menu from 'material-ui/Menu';
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Checkbox from '../../Input/Checkbox';
 import style from './styles.css';
 
 export default class Field extends Component {
     constructor(props) {
         super(props);
         this.handleOrder = this.handleOrder.bind(this);
+        this.handleMenuOpen = this.handleMenuOpen.bind(this);
+        this.handleMenuClose = this.handleMenuClose.bind(this);
+        this.toggleColumnVisibility = this.toggleColumnVisibility.bind(this);
+        this.state = {menuOpened: false};
     }
     getStyle(name) {
         return this.props.externalStyle[name] || this.context.implementationStyle[name] || style[name];
@@ -14,16 +24,76 @@ export default class Field extends Component {
     handleOrder() {
         this.props.handleOrder(this.props.field.name);
     }
+    handleMenuOpen(event) {
+        event.preventDefault();
+
+        this.setState({
+            menuOpened: true,
+            anchorEl: event.currentTarget
+        });
+    }
+    handleMenuClose() {
+        this.setState({
+            menuOpened: false
+        });
+    }
+    toggleColumnVisibility(field) {
+        return () => {
+            this.handleMenuClose();
+            this.props.toggleColumnVisibility(field);
+        };
+    }
+    getMenuColumnItems() {
+        return this.props
+            .fields
+            .filter((f) => (!f.internal))
+            .map((f) => (
+                <MenuItem
+                  children={
+                      <div onTouchTap={this.toggleColumnVisibility(f)}>
+                        <Checkbox isDisabled={false} checked={f.visible} />
+                        <span>{f.title}</span>
+                      </div>
+                  }
+                />
+            ));
+    }
+    getMenu() {
+        return (
+            <Popover
+              open={this.state.menuOpened}
+              anchorEl={this.state.anchorEl}
+              anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+              targetOrigin={{horizontal: 'left', vertical: 'top'}}
+              onRequestClose={this.handleMenuClose}
+              animation={PopoverAnimationVertical}
+            >
+                <Menu>
+                    <Divider />
+                    <MenuItem
+                      primaryText='Columns'
+                      rightIcon={<ArrowDropRight />}
+                      menuItems={this.getMenuColumnItems()}
+                    />
+                </Menu>
+            </Popover>
+        );
+    }
     render() {
-        let orderByStyle = [this.getStyle('girdOrderHeadingStyle')];
+        if (this.props.field.visible === false) {
+            return false;
+        }
+        let styles = [this.getStyle('girdOrderHeadingStyle')];
         let {orderDirection} = this.props;
 
         if (orderDirection) {
-            orderByStyle.push(this.getStyle('girdOrderHeadingStyle' + orderDirection));
+            styles.push(this.getStyle('girdOrderHeadingStyle' + orderDirection));
         }
         return (
-            <th onTouchTap={this.handleOrder}>
-                <span className={classnames.apply(undefined, orderByStyle)}>{this.props.field.title || ''}</span>
+            <th className={this.getStyle('girdHeading')}>
+                <span onTouchTap={this.handleMenuOpen} className={this.getStyle('girdHeadingMenu')} />
+                {this.getMenu()}
+                <span onTouchTap={this.handleOrder} className={classnames.apply(undefined, styles)}>{this.props.field.title || ''}</span>
             </th>
         );
     }
@@ -31,15 +101,18 @@ export default class Field extends Component {
 
 Field.propTypes = {
     field: propTypeField,
+    fields: PropTypes.array,
     externalStyle: PropTypes.object,
     orderDirection: PropTypes.string,
+    toggleColumnVisibility: PropTypes.func,
     handleOrder: PropTypes.func
 };
 
 Field.defaultProps = {
     field: {},
     orderBy: [],
-    handleOrder: (field) => ({})
+    handleOrder: (field) => ({}),
+    toggleColumnVisibility: (field) => ({})
 };
 
 Field.contextTypes = {
