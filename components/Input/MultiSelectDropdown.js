1d6df935c6304f9a4c3f9e5be548fbcdc94968a0
Kristiyan Todorov <Kristiyan.Todorov@Softwaregroup-bg.com>
2016-12-27 12:00:02 +0200

    fix: refactor multiselect dropdown

--- components/Input/MultiSelectDropdown.js
+++ components/Input/MultiSelectDropdown.js
@@ -1,11 +1,13 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import classnames from 'classnames';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import style from './style.css';
 
-class MultiSelectDropdown extends Component {
+import Dropdown from './Dropdown';
+
+class MultiSelectDropdown extends Dropdown {
 
     constructor(props) {
         super(props);
@@ -16,20 +18,28 @@ class MultiSelectDropdown extends Component {
             valid: {isValid: this.props.isValid, errorMessage: this.props.errorMessage}
         };
 
-        this.toggle = this.toggle.bind(this);
+        this.toggleOpen = this.toggleOpen.bind(this);
+        this.toggleClose = this.toggleClose.bind(this);
         this.getMenuItems = this.getMenuItems.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.renderDropDown = this.renderDropDown.bind(this);
     }
 
-    componentWillReceiveProps(props) {
-        if (this.state.values !== props.defaultSelected) {
-            this.setState({values: props.defaultSelected});
+    componentWillReceiveProps({defaultSelected, isValid, errorMessage}) {
+        if (this.state.valid.isValid !== isValid) {
+            this.setState({valid: {isValid: isValid, errorMessage: errorMessage}});
+        }
+        if (this.state.values !== defaultSelected) {
+            this.setState({values: defaultSelected});
         }
     }
 
-    toggle(event) {
-        return this.setState({open: !this.state.open});
+    toggleOpen(event) {
+        return this.setState({open: true, anchorEl: event.currentTarget});
+    }
+
+    toggleClose() {
+        return this.setState({open: false});
     }
 
     handleChange(itemKey) {
@@ -88,97 +98,49 @@ class MultiSelectDropdown extends Component {
 
         let selectedItems = this.state.values.map((value) => (value.name)).join(', ');
         if (selectedItems.length > 40) {
-            selectedItems = selectedItems.slice(0, 40) + '...';
+            selectedItems = selectedItems.slice(0, this.props.placeholderMaxLength) + '...';
         }
         return (
-            <div className={classnames(ddstyles.dropdownWrap)}>
-                <div className={classnames(iconBackground, ddstyles.dropDownRoot)} onClick={!this.props.disabled && this.toggle} >
-                    {selectedItems || this.props.placeholder}
+            <div className={classnames(ddstyles.dropdownWrap, ddstyles.pointer)} onClick={!this.props.disabled && this.toggleOpen}>
+                <div className={classnames(iconBackground, ddstyles.dropDownRoot)} >
+                    <div className={ddstyles.multiSelectDropdownPlaceholder}>
+                        {selectedItems || this.props.placeholder}
+                    </div>
                     <svg className={classnames(arrowIconDisabled, ddstyles.arrowIcon, ddstyles.dropdownIconWrap)} />
                 </div>
+                <div className={ddstyles.hideTextWrap} />
+
                 <Popover
                   open={this.state.open}
-                  onRequestClose={this.toggle}
+                  onRequestClose={this.toggleClose}
                   anchorEl={this.state.anchorEl}
-                  anchorOrigin={{horizontal: 'middle', vertical: 'top'}}
-                  targetOrigin={{horizontal: 'middle', vertical: 'top'}}
-                  style={{height: '300px'}}
+                  anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                  targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                  style={{height: '300px', cursor: 'pointer'}}
                 >
                     <Menu
+                      disableAutoFocus
                       multiple
                       value={this.state.values.map((value) => (value.key))}
-                      selectedMenuItemStyle={{color: '#2881ec'}}>
+                      selectedMenuItemStyle={this.props.selectedItemStyle}>
                         {menuItems}
                     </Menu>
                 </Popover>
             </div>
         );
     }
-
-    render() {
-        let { label, cssStyle, mergeStyles } = this.props;
-        let ddstyles = mergeStyles ? Object.assign({}, style, mergeStyles) : cssStyle || style;
-        let { isValid, errorMessage } = this.state.valid;
-        let invalidStyle = isValid ? ddstyles.hiddenHeight : '';
-
-        if (label) {
-            return (
-                <div>
-                    <div className={ddstyles.outerWrap}>
-                        <div className={ddstyles.lableWrap}>
-                            {this.props.label}
-                        </div>
-                        <div className={classnames(ddstyles.inputWrap)}>
-                            {this.renderDropDown()}
-                            <div className={classnames(ddstyles.errorWrap, invalidStyle)}>{!isValid && <div className={ddstyles.errorMessage}>{errorMessage}</div>}</div>
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <div className={ddstyles.outerWrap}>
-                        {this.renderDropDown()}
-                    </div>
-                    <div className={classnames(ddstyles.errorWrap, invalidStyle)}>{!isValid && <div className={ddstyles.errorMessage}>{errorMessage}</div>}</div>
-                </div>
-            );
-        }
-    }
 }
 
 MultiSelectDropdown.propTypes = {
-    data: PropTypes.arrayOf(PropTypes.shape({
-        key: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-        name: PropTypes.any.isRequired
-    })).isRequired,
-    defaultSelected: PropTypes.any,
-    label: PropTypes.string,
-    placeholder: PropTypes.any,
-    keyProp: PropTypes.string,
-    onSelect: PropTypes.func,
-    disabled: PropTypes.bool,
-    mergeStyles: PropTypes.object,
-    // if you want to style the width of the dropdown, add a wrapper element that has min-width and max-width properties
-    cssStyle: PropTypes.any, // css file to take styles [Should have the same classes/divs like in styles.css]
-
-    // Validation
-    isValid: PropTypes.bool,
-    errorMessage: PropTypes.string,
-
-    // Edited
-    isEdited: PropTypes.bool
+    placeholderMaxLength: PropTypes.number,
+    selectedItemStyle: PropTypes.object
 };
 
 MultiSelectDropdown.defaultProps = {
-    label: undefined,
-    placeholder: 'Select',
-    keyProp: '__no_source_prop_key__',
-    onSelect: () => {},
     isValid: true,
     errorMessage: '',
-    isEdited: false
+    placeholderMaxLength: 40,
+    selectedItemStyle: {color: '#2881ec'}
 };
 
 export default MultiSelectDropdown;
