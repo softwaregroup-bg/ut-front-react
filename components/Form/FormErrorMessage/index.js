563a010f0c72ce6de5db961e0b5bd0b1b21b8949
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-01-04 14:26:59 +0200

    fix: UIS-523: Fix lint

--- components/Form/FormErrorMessage/index.js
+++ components/Form/FormErrorMessage/index.js
@@ -1,13 +1,10 @@
 import React, { PropTypes } from 'react';
-import Icon from '../../../components/Icon';
 import styles from './styles.css';
-import { getClass } from '../../../utils/helpers';
 
-// TODO: refactor
 const FormErrorMessage = ({ message }) => {
     return (
         <div className={styles.formError}>
-            <div className={styles.errorIcon}></div>
+            <div className={styles.errorIcon} />
             <div className={styles.errorMessage}>{message}</div>
         </div>
     );
