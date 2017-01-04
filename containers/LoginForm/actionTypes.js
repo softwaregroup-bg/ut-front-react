0cf7684b347e06d98543ba75e90741b6e9a493c8
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-01-04 14:55:49 +0200

    UIS-523: Add actionTypes to LoginForm

---
+++ containers/LoginForm/actionTypes.js
@@ -0,0 +1,6 @@
+export const INIT_FORM = Symbol('INIT_FORM');
+export const LOGIN = Symbol('LOGIN');
+export const SET_INPUT_VALUE = Symbol('SET_INPUT_VALUE');
+export const VALIDATE_FORM = Symbol('VALIDATE_FORM');
+export const COOKIE_CHECK = Symbol('COOKIE_CHECK');
+export const LOGOUT = Symbol('LOGOUT');
