02ed15a4dc671c08de9e35dd0d04bee1aa91823c
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-01-04 14:23:52 +0200

    fix: UIS-523: Fix error handling

--- components/Form/FormErrorMessage/index.js
+++ components/Form/FormErrorMessage/index.js
@@ -4,13 +4,11 @@ import styles from './styles.css';
 import { getClass } from '../../../utils/helpers';
 
 // TODO: refactor
-const FormErrorMessage = ({ message, useNew }) => {
+const FormErrorMessage = ({ message }) => {
     return (
-        <div className={getClass(styles, useNew ? 'formErrorNew' : 'formError')}>
-            {!useNew ? <div className={styles.iconWrapper}>
-                            <Icon icon='error' />
-                        </div> : false }
-            <div className={getClass(styles, useNew ? 'errorMessageNew' : 'errorMessage')}>{message}</div>
+        <div className={styles.formError}>
+            <div className={styles.errorIcon}></div>
+            <div className={styles.errorMessage}>{message}</div>
         </div>
     );
 };
