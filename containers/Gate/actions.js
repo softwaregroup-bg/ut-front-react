809438115cd29f8dea1c96c52292fe7729848ea4
Kristiyan Todorov <Kristiyan.Todorov@Softwaregroup-bg.com>
2017-01-16 14:49:10 +0200

    fix: change translation fetch SP

--- containers/Gate/actions.js
+++ containers/Gate/actions.js
@@ -2,9 +2,8 @@ export const CORE_ITEM_TRANSLATION_FETCH = Symbol('CORE_ITEM_TRANSLATION_FETCH')
 
 export const fetchTranslations = (params) => ({
     type: CORE_ITEM_TRANSLATION_FETCH,
-    method: 'core.itemTranslation.fetch',
+    method: 'core.translation.fetch',
     params: {
-        ...params,
-        isEnabled: true
+        ...params
     }
 });
