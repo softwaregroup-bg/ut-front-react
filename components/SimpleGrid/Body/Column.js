c263932704ff01f91e4c79c747b6322b7e067a74
Ventsislav Arsov <Ventsislav.Arsov@Softwaregroup-bg.com>
2017-01-20 17:29:26 +0200

    fix: extend simple grid to accept className in fields

--- components/SimpleGrid/Body/Column.js
+++ components/SimpleGrid/Body/Column.js
@@ -22,7 +22,7 @@ export default class Column extends Component {
         }
 
         return (
-            <td onTouchTap={this.handleClick(value)} style={this.props.field.style} colSpan={this.props.colspan}>
+            <td onTouchTap={this.handleClick(value)} style={this.props.field.style} className={this.props.field.className} colSpan={this.props.colspan}>
                 {this.props.transformValue(value, this.props.field, this.props.data)}
             </td>
         );
