ae37aa1c15973f6252f8489557acf046c2a0a5de
Elitsa Ivanova <elitsa.ivanova@softwaregroup-bg.com>
2017-01-25 10:34:08 +0200

    finish buttoninput component

--- components/Input/ButtonInput.js
+++ components/Input/ButtonInput.js
@@ -1,29 +1,73 @@
 import React, { PropTypes, Component } from 'react';
+import classNames from 'classnames';
+import StandardButton from '../StandardButton';
 import style from './style.css';
 
 class ButtonInput extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            value: props.value
+        };
+        this.onChange = this.onChange.bind(this);
+    }
     getValue() {
         return this.refs.input.value;
     }
+    onChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+        this.props.onChange && this.props.onChange(e.target.value);
+    }
     render() {
+        const { placeholder } = this.props;
         return (
             <div className={style.outerWrap}>
-                <div className={style.lableWrap}>
+                <div>
                     {this.props.label}
                 </div>
                 <div className={style.inputWrap}>
-                    <input ref='input' className={style.input} {...this.props} style={{width: '80%'}} />
-                    <button className={style.changeBtn}>{this.props.btnText}</button>
+                    <input
+                      readOnly={this.props.readOnly}
+                      type={this.props.type}
+                      ref='input' name={this.props.name}
+                      className={classNames(style.input, style.buttonInput)}
+                      label={this.props.label}
+                      value={this.state.value}
+                      placeholder={placeholder}
+                      onChange={this.onChange} />
+                    <StandardButton
+                      type='password'
+                      className='standardBtn standardInputButton'
+                      onClick={this.props.onClick}
+                      label={this.props.btnText} />
                 </div>
             </div>
         );
     }
 }
 
+ButtonInput.defaultProps = {
+    label: '',
+    value: '',
+    name: '',
+    btnText: '',
+    placeholder: '',
+    type: 'text',
+    readOnly: false
+};
+
 ButtonInput.propTypes = {
     label: PropTypes.string,
+    readOnly: PropTypes.bool,
     value: PropTypes.string,
-    btnText: PropTypes.string
+    type: PropTypes.string,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    btnText: PropTypes.string,
+    onClick: PropTypes.func,
+    onChange: PropTypes.func
 };
 
 export default ButtonInput;
