2299e5c23c016be64e4ca94f59b0b2f1df01d786
Elitsa Ivanova <elitsa.ivanova@softwaregroup-bg.com>
2017-02-01 17:32:09 +0200

    pass external classnames to standard button

--- components/StandardButton/index.js
+++ components/StandardButton/index.js
@@ -1,21 +1,28 @@
 import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 import buttonStyles from './styles.css';
 import { getClass } from '../../utils/helpers';
+
+const getClassInternal = (className) => buttonStyles[className] || getClass(buttonStyles, className) || className;
 
 const Button = ({
     type,
     label,
     onClick,
     className
-}) => (
-    <button type={type} className={getClass(buttonStyles, className)} onClick={onClick}>
+}) => {
+    let cssClass = Array.isArray(className) ? className.map(getClassInternal) : getClassInternal(className);
+
+    return (
+      <button type={type} className={classNames(cssClass)} onClick={onClick}>
         {label}
-    </button>
-);
+      </button>
+    );
+};
 
 Button.propTypes = {
     type: PropTypes.string,
-    className: PropTypes.string,
+    className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
     label: PropTypes.string,
     onClick: PropTypes.func
 };
