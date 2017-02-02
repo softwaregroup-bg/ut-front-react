ee0c6e122049b36c5747caa33942ff4b56c78b03
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-02-02 15:51:32 +0200

    feat: UIS-1022

--- components/AddFilePopup/index.js
+++ components/AddFilePopup/index.js
@@ -1,58 +1,38 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import addFilePopupStyles from './styles.css';
 import Popup from '../Popup';
 import AddFileMenu from '../AddFileMenu';
 import FileUpload from '../FileUpload';
 
-export default class AddFilePopup extends Component {
-    constructor(props) {
-        super(props);
-
-        this.initialState = {
-            addFileOption: 'initial',
-            file: null,
-            showPreview: false,
-            actionButtons: [{
-                name: 'cancel',
-                label: 'Cancel',
-                className: 'actionBtn',
-                onClick: this.props.closePopup
-            }]
-        };
-
-        this.state = Object.assign({}, this.initialState);
-
-        this.onAddFile = this.onAddFile.bind(this);
-    }
-
-    onAddFile(addFileOption, file) {
-        this.setState({
-            addFileOption,
-            file
-        });
-    }
-    componentWillReceiveProps(nextProps) {
-        // This is done in order to reset the component's state because it doesn't get unmounted
-        if (this.props.isOpen && !nextProps.isOpen) {
-            this.setState(this.initialState);
-        }
-    }
-    render() {
-        let { isOpen, closePopup, hasOverlay, headerText, actionButtons } = this.props;
-
-        return (
-            <Popup className={addFilePopupStyles.addFilePopup}
-              isOpen={isOpen}
-              closePopup={closePopup}
-              hasOverlay={hasOverlay}
-              headerText={headerText}
-              actionButtons={actionButtons || this.state.actionButtons}>
-                {this.state.addFileOption === 'initial'? <AddFileMenu onAddFile={this.onAddFile} /> :
-                <FileUpload file={this.state.file} uploadType={this.state.addFileOption} />}
-            </Popup>
-        );
-    }
-}
+const AddFilePopup = ({
+    isOpen,
+    hasOverlay,
+    headerClass,
+    headerText,
+    footerClass,
+    buttonsClass,
+    closeOnOverlayClick,
+    actionButtons,
+    file,
+    addFileOption,
+    addFile,
+    closePopup
+}) => (
+    <Popup className={addFilePopupStyles.addFilePopup}
+      isOpen={isOpen}
+      hasOverlay={hasOverlay}
+      headerClass={headerClass}
+      headerText={headerText}
+      footerClass={footerClass}
+      buttonsClass={buttonsClass}
+      closeOnOverlayClick={closeOnOverlayClick}
+      closePopup={closePopup}
+      actionButtons={actionButtons}>
+        {children}
+        {addFileOption === 'initial'? <AddFileMenu onAddFile={addFile} /> :
+        <FileUpload file={file} uploadType={addFileOption} />}
+    </Popup>
+);
 
 AddFilePopup.propTypes = {
     isOpen: PropTypes.bool,
@@ -61,3 +41,5 @@ AddFilePopup.propTypes = {
     actionButtons: PropTypes.array,
     closePopup: PropTypes.func
 };
+
+export default AddFilePopup;
---
+++ components/FilePreview/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FilePreview extends Component {
+    render() {
+        return (
+            <div>
+            preview
+            </div>
+        );
+    }
+}
