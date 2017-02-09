c42189f04448ab268732f15e04c005f46bb1d6b7
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-02-09 13:47:05 +0200

    feat: UIS-1022: Move FileUpload Component to ut-front-react

---
+++ components/FileUpload/AddFileButton.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import classnames from 'classnames';
+import fileUploadStyles from './styles.css';
+
+const AddFileButton = ({
+    className,
+    label,
+    children,
+    onClick
+}) => (
+    <div className={classnames(fileUploadStyles.addFileBtn, className)} onClick={onClick}>
+        <span>{label}</span>
+        {children}
+    </div>
+);
+
+AddFileButton.propTypes = {
+    className: PropTypes.string,
+    label: PropTypes.string,
+    onClick: PropTypes.func,
+    children: PropTypes.object
+};
+
+export default AddFileButton;
