ab9e4bdcf0043c611664cc3cec99830f36017796
Lyubomir Georgiev <Lyubomir.Georgiev@Softwaregroup-bg.com>
2017-02-17 18:04:36 +0200

    refactor: UIS-1217 refactor the vertical component to calculate fixed height

--- components/Layout/Vertical.js
+++ components/Layout/Vertical.js
@@ -1,20 +1,46 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import style from './verticalStyles.css';
 
-const Vertical = (props) => {
-    let height = props.fixedComponentHeight + 'px';
-    return (
-        <div className={style.verticalWrapper}>
-            <div style={{height: height}}>{props.fixedComponent}</div>
-            <div style={{height: 'calc(100% - ' + height + ')'}} className={style.vertical}>{props.children}</div>
-        </div>
-    );
+class Vertical extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fixedComponent: 0
+        };
+        this.updateHeight = this.updateHeight.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateHeight();
+    }
+
+    componentDidUpdate() {
+        this.updateHeight();
+    }
+
+    updateHeight() {
+        let { clientHeight } = this.refs.fixedComponent;
+        if (clientHeight !== this.state.fixedComponent) {
+            this.setState({
+                fixedComponent: clientHeight
+            });
+        }
+    }
+
+    render() {
+        let height = this.state.fixedComponent + 'px';
+        return (
+            <div className={style.verticalWrapper}>
+                <div ref='fixedComponent'>{this.props.fixedComponent}</div>
+                <div style={{height: 'calc(100% - ' + height + ')'}} className={style.vertical}>{this.props.children}</div>
+            </div>
+        );
+    }
 };
 
 Vertical.propTypes = {
     children: PropTypes.any,
-    fixedComponent: PropTypes.any.isRequired,
-    fixedComponentHeight: PropTypes.number.isRequired // pixels
+    fixedComponent: PropTypes.any.isRequired
 };
 
 export default Vertical;
