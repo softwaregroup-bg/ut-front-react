d9edf5422c30d23e2569067e0951725b71008637
Kristiyan Todorov <Kristiyan.Todorov@Softwaregroup-bg.com>
2017-02-24 17:13:03 +0200

    fix: datetime picker between range

--- components/DateTimePicker/Between/index.js
+++ components/DateTimePicker/Between/index.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import DateTimePicker from '../Simple';
 
 class DateTimePickerBetween extends Component {
     render() {
+        let {
+            DateTimeFormat,
+            locale,
+            labelFrom,
+            labelTo,
+            okLabel,
+            cancelLabel,
+            firstDayOfWeek,
+            onChange,
+            defaultValue
+        } = this.props;
+
         return (
             <div>
                 <div>
-                    <DateTimePicker />
+                    <DateTimePicker
+                      defaultValue={defaultValue.from}
+                      DateTimeFormat={DateTimeFormat}
+                      locale={locale}
+                      label={labelFrom}
+                      okLabel={okLabel}
+                      cancelLabel={cancelLabel}
+                      firstDayOfWeek={firstDayOfWeek}
+                      onChange={({value}) => { onChange({key: 'from', value: value}); }}
+                      boldLabel={this.props.boldLabel} />
                 </div>
                 <div>
-                    <DateTimePicker />
+                    <DateTimePicker
+                      defaultValue={defaultValue.to}
+                      DateTimeFormat={DateTimeFormat}
+                      locale={locale}
+                      label={labelTo}
+                      okLabel={okLabel}
+                      cancelLabel={cancelLabel}
+                      firstDayOfWeek={firstDayOfWeek}
+                      onChange={({value}) => { onChange({key: 'to', value: value}); }}
+                      boldLabel={this.props.boldLabel} />
                 </div>
             </div>
         );
@@ -18,7 +48,17 @@ class DateTimePickerBetween extends Component {
 }
 
 DateTimePickerBetween.propTypes = {
-
+    defaultValue: PropTypes.object,
+    locale: PropTypes.string,
+    okLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    cancelLabel: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    firstDayOfWeek: PropTypes.number,
+    withVerticalClass: PropTypes.bool,
+    labelFrom: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    labelTo: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    DateTimeFormat: PropTypes.func,
+    onChange: PropTypes.func,
+    boldLabel: PropTypes.bool
 };
 
 export default DateTimePickerBetween;
