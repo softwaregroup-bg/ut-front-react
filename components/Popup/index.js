3090dee84f8d4e4e6dcb999b81785949c4c16613
Mariela Kasovska <mariela.kasovska@softwaregroup-bg.com>
2017-02-24 18:33:57 +0200

    feat: UIS-1022: Remove Portal

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -1,11 +1,11 @@
-import React, { PropTypes } from 'react';
-import { Portal } from 'react-overlays';
+import React, { Component, PropTypes } from 'react';
+import RenderToLayer from './RenderToLayer';
 import classnames from 'classnames';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import styles from './styles.css';
 
-const Popup = ({
+const PopupInternal = ({
     isOpen,
     container,
     className,
@@ -16,21 +16,69 @@ const Popup = ({
     children,
     closePopup
 }) => {
-    /* The Portal component renders its children into a new subtree outside of current component hierarchy. */
-    /* Its children will be appended to the [container] specified (default: div#controls, located in the Layout component) */
     return (
-        <Portal container={() => { return document.getElementById(container); }}>
-            { isOpen && <div className={styles.modalContainer}>
-                { hasOverlay && <div className={styles.modalOverlay} onClick={closeOnOverlayClick ? closePopup : null} /> }
-                <div className={classnames(styles.popupContainer, className)}>
-                    { header && <Header className={header.className} text={header.text} closePopup={closePopup} /> }
-                    { children }
-                    { footer && <Footer className={footer.className} actionButtons={footer.actionButtons} /> }
-                </div>
-            </div> }
-        </Portal>
+        <div className={styles.modalContainer}>
+            { hasOverlay && <div className={styles.modalOverlay} onClick={closeOnOverlayClick ? closePopup : null} /> }
+            <div className={classnames(styles.popupContainer, className)}>
+                { header && <Header className={header.className} text={header.text} closePopup={closePopup} /> }
+                { children }
+                { footer && <Footer className={footer.className} actionButtons={footer.actionButtons} /> }
+            </div>
+        </div>
     );
 };
+
+PopupInternal.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    container: PropTypes.string,
+    className: PropTypes.string,
+    hasOverlay: PropTypes.bool,
+    closeOnOverlayClick: PropTypes.bool,
+    header: PropTypes.shape({
+        className: PropTypes.string,
+        text: PropTypes.string,
+        closePopup: PropTypes.func
+    }),
+    footer: PropTypes.shape({
+        className: PropTypes.string,
+        actionButtons: PropTypes.arrayOf(PropTypes.shape({
+            className: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+            label: PropTypes.string,
+            onClick: PropTypes.func
+        }))
+    }),
+    children: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.object
+    ]),
+    closePopup: PropTypes.func
+};
+
+PopupInternal.defaultProps = {
+    hasOverlay: true,
+    container: 'controls',
+    closeOnOverlayClick: false
+};
+
+class Popup extends Component {
+    constructor() {
+        super();
+
+        this.renderLayer = this.renderLayer.bind(this);
+    }
+
+    renderLayer() {
+        return <PopupInternal {...this.props} />;
+    }
+
+    render() {
+        const { isOpen } = this.props;
+
+        return (
+            <RenderToLayer render={this.renderLayer} open={isOpen} />
+        );
+    }
+}
 
 Popup.propTypes = {
     isOpen: PropTypes.bool.isRequired,
