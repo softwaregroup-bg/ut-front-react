c5ceecfd58848af374ec59ea22a3f1ba4925f8af
Emilia Pavlova <emiliya.pavlova@softwaregroup-bg.com>
2017-04-06 14:51:43 +0300

    add capitalize every work function

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,3 +7,5 @@ export const breakOnSpaceChars = (str) => str.split('\\n');
 export const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
 export const generateUniqueId = () => `${Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}_${Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}`;
+
+export const caitalizeEveryWord = (string) => string.split(' ').map((word) => capitalizeFirstLetter(word)).join(' ');
