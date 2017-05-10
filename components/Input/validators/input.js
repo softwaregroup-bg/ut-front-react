e1ee6a45e734185d673b35801ad935d2246b47dc
Nedyalko Nikolov <nedyalko.nikolov@softwaregroup-bg.com>
2017-05-10 10:21:42 +0300

    feat: Add Integer and IntegerRange validator.

--- components/Input/validators/input.js
+++ components/Input/validators/input.js
@@ -1,5 +1,5 @@
 import { textValidations } from '../../../validator/constants';
-import { isRequiredRule, lengthRule, regexRule, isNumberOnlyRule, isDecimalOnlyRule, isValidEmailRule, isUniqueValueRule } from '../../../validator';
+import { isRequiredRule, lengthRule, regexRule, isNumberOnlyRule, isIntegerOnlyRule, isIntegerRangeRule, isDecimalOnlyRule, isValidEmailRule, isUniqueValueRule } from '../../../validator';
 
 export default (valueToValidate, validators) => {
     let result = { isValid: true, errors: [] };
@@ -9,6 +9,8 @@ export default (valueToValidate, validators) => {
         if (validator.type === textValidations.length) lengthRule(valueToValidate, validator.minVal, validator.maxVal, validator, result);
         if (validator.type === textValidations.regex) regexRule(valueToValidate, validator.value, validator, result);
         if (validator.type === textValidations.numberOnly) isNumberOnlyRule(valueToValidate, validator, result);
+        if (validator.type === textValidations.integerOnly) isIntegerOnlyRule(valueToValidate, validator, result);
+        if (validator.type === textValidations.integerRange) isIntegerRangeRule(valueToValidate, validator, result);
         if (validator.type === textValidations.decimalOnly) isDecimalOnlyRule(valueToValidate, validator.precision, validator.scale, validator, result);
         if (validator.type === textValidations.email) isValidEmailRule(valueToValidate, validator, result);
         if (validator.type === textValidations.uniqueValue) isUniqueValueRule(valueToValidate, validator.values, validator, result);
