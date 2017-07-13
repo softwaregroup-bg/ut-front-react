a5a90ef6812bcf1a89a0f01224f960a073d77ca1
Simeon Petkov <simeon.petkov@softwaregroup-bg.com>
2017-07-13 16:45:21 +0300

    ABT-1851: updated error window

--- components/ErrorWindow/index.js
+++ components/ErrorWindow/index.js
@@ -1,27 +1,60 @@
 import React, { PropTypes } from 'react';
-import Dialog from 'material-ui/Dialog/Dialog';
-import FlatButton from 'material-ui/FlatButton';
+import Popup from '../Popup';
 
-const ErrorWindow = ({open = true, message, close, title}) => {
+import styles from './style.css';
+import formErrorMessageStyles from '../Form/FormErrorMessage/styles.css';
+import classnames from 'classnames';
+
+const ErrorWindow = ({open, message, close, title, type}) => {
+    let closePopUpHandler = close;
+    let header = {text: title}
+    let actionButtons = [
+        {
+            label: 'Close',
+            styleType: 'primaryDialog',
+            disabled: false,
+            onClick: closePopUpHandler
+        }
+    ];
+
+    if (type === 'identity.invalidCredentials') {
+        closePopUpHandler = undefined;
+        header.closeIcon = false; // remove close icon
+        actionButtons = [
+            {
+                label: 'Go to login',
+                styleType: 'primaryDialog', // secondaryDialog
+                href: '/login' // migh be a good idea to pass it from outside. However, the http server handles each request, in case of invalid session redirects to the accroding login page
+            }
+        ];
+    };
+
     return (
-      <Dialog
-        style={{zIndex: 9999}}
-        open={open}
-        title={title || 'ERROR'}
-        actions={[
-            <FlatButton label='Close' keyboardFocused onTouchTap={close} />
-        ]}
-      >
-        {message}
-      </Dialog>
-    );
+      <Popup
+        isOpen={open}
+        header={header}
+        footer={{ actionButtons: actionButtons }}
+        closePopup={closePopUpHandler} >
+          <div className={classnames(styles.errorIconWrap, formErrorMessageStyles.errorIcon)} />
+          <div className={styles.errorMessageWrap}>
+            {message}
+          </div>
+      </Popup>
+  );
 };
 
 ErrorWindow.propTypes = {
     open: PropTypes.bool,
     close: PropTypes.func,
     message: PropTypes.node,
-    title: PropTypes.string
+    title: PropTypes.string,
+    type: PropTypes.string
+};
+
+ErrorWindow.defaultProps = {
+    open: true,
+    title: 'ERROR',
+    type: ''
 };
 
 export default ErrorWindow;
