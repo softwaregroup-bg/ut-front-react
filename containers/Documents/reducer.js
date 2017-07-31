374c141eced5263072d18c12adce681ad1593dea
Lyubomir Georgiev <Lyubomir.Georgiev@Softwaregroup-bg.com>
2017-07-31 18:07:17 +0300

    fix: UIS-2746 display the correct property for document type

--- containers/Documents/reducer.js
+++ containers/Documents/reducer.js
@@ -84,7 +84,7 @@ const documents = (state = defaultState, action) => {
                     let data = action.result.documentTypeClass.map((type) => {
                         return {
                             key: type.id,
-                            name: type.description
+                            name: type.name
                         };
                     });
                     return state.setIn([props.identifier, 'documentTypes', 'requiresFetch'], Immutable.fromJS(false))
