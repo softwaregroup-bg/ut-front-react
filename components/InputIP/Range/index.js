85e043d413e54eedeb0cf197400958618e3c3901
Lawanya Selvamani <Lawanya.Selvamani@Softwaregroup-bg.com>
2018-01-03 14:13:25 +0530

    feat: update range component

--- components/InputIP/Range/index.js
+++ components/InputIP/Range/index.js
@@ -1,5 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import IPInput from '../Simple';
+import Input from '../../Input';
+import { textValidations } from '../../../validator/constants';
 import classnames from 'classnames';
 import style from './style.css';
 
@@ -25,8 +27,8 @@ class Range extends Component {
             errorMessageGeneral = '';
         }
         return (
-            <div className={style.rangeWrap}>
-                <div>
+            <div className={this.props.inputType === 'ipRange'? style.rangeWrap : style.textRangeWrap}>
+                {this.props.inputType === 'ipRange' ? <div>
                     <div className={style.wrapper}>
                         <IPInput
                           value={this.props.valueFrom}
@@ -50,7 +52,41 @@ class Range extends Component {
                           placeholder={this.props.placeholderTo}
                         />
                     </div>
-                </div>
+                </div> : this.props.inputType === 'textRange' ?
+                <div className={style.textWrapper}>
+                    <div className={classnames(style.textRangeSeperator, this.props.inputWrapper)}>
+                        <Input
+                          value={this.props.value.from}
+                          onChange={this.props.onChangeFrom}
+                          isValid={isValidFrom}
+                          readonly={this.props.readonlyFrom}
+                          errorMessage={errorMessageFrom}
+                          placeholder={this.props.placeholderFrom}
+                          keyProp={this.props.keyProp.from}
+                          validators={this.props.validators}
+                          label={this.props.labelFrom}
+                          labelClassName={this.props.labelClassName || style.labelWrapper}
+                          inputWrapClassName={this.props.inputWrapClassName}
+                          boldLabel={this.props.boldLabel}
+                        />
+                    </div>
+                    <div className={classnames(style.textRangeSeperator, style.spaceWrap, this.props.inputWrapper)}>
+                        <Input
+                          value={this.props.value.to}
+                          onChange={this.props.onChangeTo}
+                          isValid={isValidTo}
+                          readonly={this.props.readonlyTo}
+                          errorMessage={errorMessageTo}
+                          placeholder={this.props.placeholderTo}
+                          keyProp={this.props.keyProp.to}
+                          validators={this.props.validators}
+                          label={this.props.labelTo}
+                          labelClassName={this.props.labelClassName || style.toLabelWrapper}
+                          inputWrapClassName={this.props.inputWrapClassName}
+                          boldLabel={this.props.boldLabel}
+                        />
+                    </div>
+                </div> : ''}
                 <div className={classnames(style.errorWrap, zeroHeightStyle)}>{!this.props.isValidGeneral && <div className={style.errorMessage}>{errorMessageGeneral}</div>}</div>
             </div>
         );
@@ -58,6 +94,11 @@ class Range extends Component {
 }
 
 Range.propTypes = {
+    inputType: PropTypes.string,
+    keyProp: PropTypes.shape({
+        from: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+        to: PropTypes.oneOfType([PropTypes.string, PropTypes.array])
+    }),
     valueFrom: PropTypes.string,
     onChangeFrom: PropTypes.func,
     isValidFrom: PropTypes.bool,
@@ -77,17 +118,37 @@ Range.propTypes = {
         PropTypes.array
     ]),
     isValidGeneral: PropTypes.bool,
-    errorMessageGeneral: PropTypes.string
+    labelFrom: PropTypes.node,
+    labelTo: PropTypes.node,
+    errorMessageGeneral: PropTypes.string,
+    inputWrapClassName: PropTypes.string,
+    labelClassName: PropTypes.string,
+    inputWrapper: PropTypes.string,
+    value: PropTypes.shape({
+        from: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        to: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    }),
+    validators: PropTypes.arrayOf(
+        PropTypes.shape({
+            type: PropTypes.oneOf([textValidations.isRequired, textValidations.length, textValidations.numberOnly, textValidations.decimalOnly, textValidations.email, textValidations.uniqueValue, textValidations.regex]),
+            values: PropTypes.any,
+            errorMessage: PropTypes.string
+        })
+    ),
+    boldLabel: PropTypes.bool,
 };
 
 Range.defaultProps = {
+    inputType: 'ipRange',
     isValidFrom: true,
     errorMessageFrom: '',
     placeholderFrom: '...',
     isValidTo: true,
     errorMessageTo: '',
     placeholderTo: '...',
-    isValidGeneral: true
+    isValidGeneral: true,
+    labelFrom: 'From',
+    labelTo: 'To'
 };
 
 export default Range;
