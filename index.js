6f01d07f70a6a4541dceae87d895e753648c5e8b
elin.angelov <elin.angelov@softwaregroup-bg.com>
2018-02-12 18:48:46 +0200

    fix: remove bootstrap deps and explicit require;rm script tag

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ module.exports = {
     start: function() {
         return this && this.registerRequestHandler && this.registerRequestHandler([{
             method: 'GET',
-            path: '/static/assets/bootstrap/{p*}',
-            config: {auth: false},
-            handler: {
-                directory: {
-                    path: path.resolve(require.resolve('bootstrap'), '../..'),
-                    listing: false,
-                    index: true,
-                    lookupCompressed: true
-                }
-            }
-        },
-        {
-            method: 'GET',
             path: '/static/assets/react-select/{p*}',
             config: {auth: false},
             handler: {
