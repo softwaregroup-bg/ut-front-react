eda92117c765fde0242b3cf7d520085d1d88c991
shenbagavalli.thiyag <Shenbagavalli.thiyagarajan@softwaregroup-bg.com>
2018-03-14 17:35:19 +0530

    feat: style changes for search button UTP-1461

--- components/AdvancedSearchButton/index.js
+++ components/AdvancedSearchButton/index.js
@@ -2,12 +2,13 @@ import React, {Component, PropTypes} from 'react';
 import mainStyle from './style.css';
 import SvgSearchIcon from 'material-ui/svg-icons/action/search';
 import styles from '../StandardButton/styles.css';
+import classnames from 'classnames';
 
 export class AdvancedSearchButton extends Component {
     render() {
         return (
             <div>
-            <button className={styles.defaultBtn} onClick={this.props.onClick}>
+            <button className={classnames(styles.defaultBtn, mainStyle.defaultButton)} onClick={this.props.onClick}>
             {<span className={mainStyle.iconWrap}>
                         <SvgSearchIcon color='#808080' style={{display: 'flex', height: '20px'}} />
                     </span>} Advanced Search </ button>
