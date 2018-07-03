a121e7e6911e5ce18151bbd8c7f732bf9e8f6eb4
ranjith.pandiyan <ranjith.pandiyan@softwaregroup-bg.com>
2018-07-03 12:16:59 +0530

    fix: change grid header cursor type

--- components/Grid/HeaderCell.js
+++ components/Grid/HeaderCell.js
@@ -19,9 +19,8 @@ class HeaderCell extends Component {
         }
 
         return (
-             <th onClick={this.props.canSort ? this.props.onSort : null} style={{width: 'auto', ...this.props.styles}}>
-                {this.props.name}
-                {this.props.canSort && <span className={sortStyle} />}
+            <th style={{width: 'auto', ...this.props.styles}}>
+                <span onClick={this.props.canSort ? this.props.onSort : null} className={style.thTitle}> {this.props.name}  {this.props.canSort && <span className={sortStyle} />} </span>
             </th>
         );
     }
