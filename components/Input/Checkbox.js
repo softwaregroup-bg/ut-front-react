3d190b2f16e690da64f760bf1210d19d9dc17613
Simeon <petkov.spp@gmail.com>
2018-08-17 13:53:40 +0300

    updated Input Checkbox component to support inherited checked values

--- components/Input/Checkbox.js
+++ components/Input/Checkbox.js
@@ -14,9 +14,13 @@ const Checkbox = (props) => {
         label = <p onTouchTap={isDisabled ? noop : props.onClick}>{props.label}</p>;
     }
     let isDisabledClass = isDisabled ? classnames(style.notAllowed, style.disabledCheckbox) : style.pointer;
+    let classStyles = classnames(style.checkBoxWrapper, isDisabledClass);
+    if (propsLeft.styleType === 'inherit') {
+        classStyles = classnames(classStyles, style.checkBoxWrapperInherit);
+    }
 
     return (
-        <span className={classnames(style.checkBoxWrapper, isDisabledClass)} >
+        <span className={classStyles} >
             <input {...propsLeft} type='checkbox' onChange={noop} className={classnames(style.checkBox, isDisabledClass)} />
             <div className={style.innerWrap}>
                 <label className={style.checkBoxWrap} onTouchTap={isDisabled ? noop : props.onClick}><span className={classnames(style.checkBoxSpanWrapper, isDisabledClass)} /></label>
@@ -30,11 +34,13 @@ Checkbox.propTypes = {
     onClick: PropTypes.func,
     label: PropTypes.string,
     checked: PropTypes.bool,
-    isDisabled: PropTypes.bool
+    isDisabled: PropTypes.bool,
+    styleType: PropTypes.oneOf(['primary', 'inherit'])
 };
 
 Checkbox.defaultProps = {
-    isDisabled: false
+    isDisabled: false,
+    styleType: 'primary'
 };
 
 export default Checkbox;
