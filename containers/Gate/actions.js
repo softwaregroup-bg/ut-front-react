720968c2c2937e14955d4a6aaf53ecb98466dab8
Stoyan Berov <stoyan.berov@softwaregroup-bg.com>
2018-11-20 10:20:31 +0200

    refactor: remove a redundant line

--- containers/Gate/actions.js
+++ containers/Gate/actions.js
@@ -1,7 +1,6 @@
 export const CORE_ITEM_TRANSLATION_FETCH = Symbol('CORE_ITEM_TRANSLATION_FETCH');
 export const CORE_ITEM_LANGUAGE_FETCH = Symbol('CORE_ITEM_LANGUAGE_FETCH');
 
-
 export const fetchTranslations = (params) => ({
     type: CORE_ITEM_TRANSLATION_FETCH,
     method: 'core.translation.fetch',
