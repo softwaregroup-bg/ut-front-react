58cc7509e994d4d8183d13c67e5adf95dd0da31f
Kalin Krustev <kalin.krustev@gmail.com>
2018-12-18 09:38:36 +0200

    fix: add common react code

--- reducers.js
+++
@@ -1,9 +0,0 @@
-import { login } from './containers/LoginForm/reducer';
-// import {login, loginData} from './pages/Login/reducer';
-// import gate from './pages/Gate/reducer';
-import gate from './containers/Gate/reducer';
-import master from './pages/Master/reducer';
-import { reducer as form } from 'redux-form/immutable';
-import frontDocuments from './containers/Documents/reducer';
-
-export default {login, /* loginData, */ gate, form, ...master, frontDocuments};
