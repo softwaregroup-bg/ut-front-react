746fced974f55747daf8fa57f83a030a827d87c7
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-01-20 13:18:02 +0200

    fix: avoid loop

--- containers/Gate/reducer.js
+++ containers/Gate/reducer.js
@@ -5,7 +5,7 @@ const defaultState = Immutable.Map({});
 
 // TODO: check translations
 export default (state = defaultState, action) => {
-    if (action.error && (action.error.type === 'identity.systemError' || action.error.statusCode === 401)) {
+    if (action.error && (action.error.type === 'identity.systemError' || (action.error.statusCode === 401 && !['identity.closeSession', 'identity.check'].includes(action.method)))) {
         return state.set('forceLogOut', true);
     } else if (action.type === CORE_ITEM_TRANSLATION_FETCH) {
         if (action.methodRequestState === 'finished') {
