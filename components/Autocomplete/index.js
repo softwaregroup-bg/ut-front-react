cd4aeaa235e93c45ab740aa7c774988071a250fc
radoslav.demirov <radoslav.demirov@softwaregroup.com>
2021-04-16 08:57:28 +0300

    feat (NSM-172) : Notifications management in PAP
    - Clear input value of the autocomplete by adding an inputValue property

--- components/Autocomplete/index.js
+++ components/Autocomplete/index.js
@@ -6,16 +6,17 @@ import TextField from '@material-ui/core/TextField';
 import style from './style.css';
 export default class SimpleAutoComplete extends Component {
     render() {
-        const {label, placeholder, size, onChange, options} = this.props;
+        const {label, placeholder, size, onChange, options, inputValue} = this.props;
         return (
             <div className={classnames(style.autoComplete)} style={this.props.wrapperStyles}>
-                <div onClick={this.props.handleButtonClick} />
+                <div />
                 <Autocomplete
                     className={style.autoCompletePopup}
                     multiple={this.props.multiple}
                     options={options}
                     getOptionLabel={(option) => option.value}
                     filterSelectedOptions
+                    inputValue={inputValue}
                     size={size}
                     onChange={onChange}
                     renderInput={(inputProps) => (
@@ -34,37 +35,22 @@ export default class SimpleAutoComplete extends Component {
 }
 
 SimpleAutoComplete.defaultProps = {
-    // filter: AutoComplete.fuzzyFilter,
-    maxSearchResults: 10,
-    openOnFocus: true,
     label: '',
     placeholder: '',
-    renderTagsProps: null,
     multiple: false,
     size: 'small',
+    inputValue: '',
     options: [],
-    onChange: () => {},
-    inputProps: {}
+    onChange: () => {}
 };
 
 SimpleAutoComplete.propTypes = {
-    open: PropTypes.bool,
-    dataSource: PropTypes.array.isRequired,
-    maxSearchResults: PropTypes.number,
-    filter: PropTypes.func.isRequired,
-    onClick: PropTypes.func,
     wrapperStyles: PropTypes.object,
-    iconStyles: PropTypes.object,
-    fullWidth: PropTypes.bool,
-    handleButtonClick: PropTypes.func,
     onChange: PropTypes.func,
-    openOnFocus: PropTypes.bool,
-    dataSourceConfig: PropTypes.object,
-    onSelect: PropTypes.func,
     options: PropTypes.array,
     multiple: PropTypes.bool,
     label: PropTypes.string,
     placeholder: PropTypes.string,
     size: PropTypes.string,
-    searchText: PropTypes.string
+    inputValue: PropTypes.string
 };
