5f4b3c88da080c0420a5016d806ab644abbc8281
Kalin Krustev <kalin.krustev@softwaregroup.com>
2021-06-18 09:32:55 +0300

    feat: disable autocomplete for login [UTCORE-121]

--- pages/Login/partials/UserName.js
+++ pages/Login/partials/UserName.js
@@ -35,7 +35,7 @@ class Username extends React.Component {
         }
         return (
             <div className={classnames('margin-bottom-25', this.getStyle('loginInputContainer'))}>
-                <input type='text' placeholder='Username' autoFocus ref='input' value={value} onChange={this.handleChange} />
+                <input type='text' placeholder='Username' autoFocus autoComplete='off' ref='input' value={value} onChange={this.handleChange} />
             </div>
         );
     }
