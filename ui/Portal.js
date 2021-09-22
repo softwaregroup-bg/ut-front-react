9e65fab809ce689faf2bfa82391e61fe6c467be6
edwin.njiru <@p0stman>
2021-09-22 10:14:29 +0300

    rebranding NMb-123

--- ui/Portal.js
+++ ui/Portal.js
@@ -5,7 +5,7 @@ import Layout from '../pages/Layout';
 export default class Portal extends React.Component {
     render() {
         return (
-            <Layout {...this.props} headerText={`${this.context.portalName}\nPortal`} />
+            <Layout {...this.props} headerText={`Wakala ${this.context.portalName}\nPortal`} />
         );
     }
 }
