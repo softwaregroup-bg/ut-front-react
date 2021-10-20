aa1f0837bb24bf1ed84b6c4959c1c8383d49fe2b
BozhidarTodorov <36701256+BozhidarTodorov@users.noreply.github.com>
2021-10-20 09:24:42 +0300

    feat(MSA-53): add option for allowing negative decimals; fix showing empty input when valus equals 0 (#585)

--- components/Input/index.js
+++ components/Input/index.js
@@ -86,8 +86,9 @@ class TextField extends Component {
         const { label, type, placeholder, onClick, onBlur, dependancyDisabledInputTooltipText, inputWrapClassName, wrapperClassName, labelClassName } = this.props;
         const { isValid, errorMessage } = this.state.valid;
         const zeroHeightStyle = isValid ? this.style.hh : '';
+        const value = this.state.value !== undefined && this.state.value !== null ? this.state.value : '';
 
-        const input = <input ref='textInput' type={type} className={classnames(this.inputClassName, this.props.classes.border)} value={this.state.value || ''} onClick={onClick} onBlur={onBlur} onChange={this.handleChange} readOnly={this.props.readonly} placeholder={placeholder} />;
+        const input = <input ref='textInput' type={type} className={classnames(this.inputClassName, this.props.classes.border)} value={value} onClick={onClick} onBlur={onBlur} onChange={this.handleChange} readOnly={this.props.readonly} placeholder={placeholder} />;
         const tooltip = (this.props.readonly && dependancyDisabledInputTooltipText && <span className={this.style.tooltiptext}> <Text>{dependancyDisabledInputTooltipText}</Text> </span>);
         if (label) {
             return (
