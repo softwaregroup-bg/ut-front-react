fc9b7205af04661a944582fc02c3d7f1e30ab5e6
radoslav.demirov <radoslav.demirov@softwaregroup.com>
2022-01-21 14:06:57 +0200

    chore (ECNT-9999) : Lint project code
    - Update ut-tools version
    - Rename property to avoid linting error

--- components/DocumentUpload/index.js
+++ components/DocumentUpload/index.js
@@ -288,7 +288,7 @@ export default class DocumentUpload extends Component {
 
     uploadFile = async(file, uploadURL = '/file-upload') => {
         const {
-            useFile,
+            useFile: attachFile,
             uploadDocument
         } = this.props;
         const data = new window.FormData();
@@ -308,7 +308,7 @@ export default class DocumentUpload extends Component {
             const reader = new window.FileReader();
             reader.onload = (data) => {
                 try {
-                    useFile({
+                    attachFile({
                         filename: attachmentResult.result.filename,
                         createdDate: new Date().toISOString(),
                         extension: ext,
