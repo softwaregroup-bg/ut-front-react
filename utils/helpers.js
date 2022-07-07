b63fa28a718d96e2f81ad549ab71e53ad452a08e
radoslavdemirov <44233628+radoslavdemirov@users.noreply.github.com>
2022-07-07 09:10:17 +0300

    feat (ECNT-1495): Default value for field
    Co-authored-by: Stoyan Berov <stoyan.berov@gmail.com>

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,7 +4,7 @@ export const getClass = (styles, className) => className && classnames(className
 
 export const breakOnSpaceChars = (str) => str.split('\\n');
 
-export const capitalizeFirstLetter = (string) => string ? string.charAt(0).toUpperCase() + string.slice(1) : string;
+export const capitalizeFirstLetter = (string = '') => string.charAt(0).toUpperCase() + string.slice(1);
 
 export const generateUniqueId = () => `${Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}_${Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}`;
 
