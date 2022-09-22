0783aba84e58229eef11c57783e35bd75c34f97c
edwin.njiru <D@rkv4dr9376>
2022-09-22 12:54:44 +0300

    sorted build NMB-123

--- ui/Dashboard.js
+++ ui/Dashboard.js
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getLink } from 'ut-front-react/routerHelper';
+import { getLink } from '../routerHelper';
 import {Dashboard} from '../containers/Dashboard';
 
 export default class NamedDashboard extends React.Component {
-    constructor(){
-        super()
+    constructor() {
+        super();
         this.myRef = React.createRef();
     }
-    componentDidMount(){
-        if(this.context.portalName === 'AgencyBanking') {
+
+    componentDidMount() {
+        if (this.context.portalName === 'AgencyBanking') {
             this.myRef?.current?.click?.();
         }
     }
+
     render() {
         return (
             <div>
-                <div style={{display:'none'}}>
-                <Link ref={this.myRef} to={getLink('ut-agent:dashboard')}>{null}</Link>
+                <div style={{display: 'none'}}>
+                    <Link ref={this.myRef} to={getLink('ut-agent:dashboard')}>{null}</Link>
                 </div>
                 {this.context.portalName !== 'AgencyBanking' && <Dashboard {...this.props} tabName='Dashboard' pageText={`${this.context.portalName} Dashboard`} />}
             </div>
