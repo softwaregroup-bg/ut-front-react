2166c64d345957e226efe8088251ddeb566eca5d
Martin Shumankov <martin.shumankov@softwaregrouo.com>
2023-09-28 17:33:35 +0300

    chore: lint

--- components/MaterialUILayout/index.js
+++ components/MaterialUILayout/index.js
@@ -3,48 +3,52 @@ import React, { useEffect, useState } from 'react';
 import * as muiStyles from '@material-ui/core/styles';
 import DateUtils from '../DateUtils';
 // the createMuiTheme function was renamed to createTheme at some point
-const {MuiThemeProvider, createMuiTheme, createTheme = createMuiTheme} = muiStyles;
+const { MuiThemeProvider, createMuiTheme, createTheme = createMuiTheme } = muiStyles;
 
 const MaterialUILayout = ({ children, utMethod }) => {
-    debugger;
-    const [theme, setTheme] = useState(createTheme())
-    useEffect(async() => {
-        const defaultColor = '#4096fd'
+    const [theme, setTheme] = useState(createTheme());
+    useEffect(() => {
+        const defaultColor = '#4096fd';
         let color;
-        try {
-            color = await utMethod('mainUI.muiThemeColor')({});
-        } catch(error) {
-            color = defaultColor
-        }
-        const theme = createTheme({
-            palette: {
-                primary: {
-                    main: color || defaultColor
-                }
-            },
-            overrides: {
-                MuiCssBaseline: {
-                    '@global': {
-                        body: {
-                            lineHeight: 1.1
+        (async function getColor() {
+            try {
+                color = await utMethod('mainUI.muiThemeColor')({});
+            } catch (error) {
+                color = defaultColor;
+            }
+
+            const theme = createTheme({
+                palette: {
+                    primary: {
+                        main: color || defaultColor
+                    }
+                },
+                overrides: {
+                    MuiCssBaseline: {
+                        '@global': {
+                            body: {
+                                lineHeight: 1.1
+                            }
                         }
                     }
                 }
-            }
-        });
-        setTheme(theme);
-    }, []);
+            });
+            setTheme(theme);
+        })();
+    }, [utMethod]);
 
     return (
-    <DateUtils>
-        <MuiThemeProvider theme={theme}>
-            {children}
-        </MuiThemeProvider>
-    </DateUtils>
-)};
+        <DateUtils>
+            <MuiThemeProvider theme={theme}>
+                {children}
+            </MuiThemeProvider>
+        </DateUtils>
+    );
+};
 
 MaterialUILayout.propTypes = {
-    children: PropTypes.any
+    children: PropTypes.any,
+    utMethod: PropTypes.func
 };
 
 export default MaterialUILayout;
