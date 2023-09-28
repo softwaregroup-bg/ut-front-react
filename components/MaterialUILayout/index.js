eb67ab0039ce476f6a83b00fcb77e2b8a20c3e98
Martin Shumankov <martin.shumankov@softwaregrouo.com>
2023-09-28 16:45:35 +0300

    feat: add option to change color in DatePicker BDC-2427

--- components/MaterialUILayout/index.js
+++ components/MaterialUILayout/index.js
@@ -1,32 +1,47 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as muiStyles from '@material-ui/core/styles';
 import DateUtils from '../DateUtils';
 // the createMuiTheme function was renamed to createTheme at some point
 const {MuiThemeProvider, createMuiTheme, createTheme = createMuiTheme} = muiStyles;
 
-const theme = createTheme({
-    palette: {
-        type: 'light'
-    },
-    overrides: {
-        MuiCssBaseline: {
-            '@global': {
-                body: {
-                    lineHeight: 1.1
+const MaterialUILayout = ({ children, utMethod }) => {
+    debugger;
+    const [theme, setTheme] = useState(createTheme())
+    useEffect(async() => {
+        const defaultColor = '#4096fd'
+        let color;
+        try {
+            color = await utMethod('mainUI.muiThemeColor')({});
+        } catch(error) {
+            color = defaultColor
+        }
+        const theme = createTheme({
+            palette: {
+                primary: {
+                    main: color || defaultColor
+                }
+            },
+            overrides: {
+                MuiCssBaseline: {
+                    '@global': {
+                        body: {
+                            lineHeight: 1.1
+                        }
+                    }
                 }
             }
-        }
-    }
-});
+        });
+        setTheme(theme);
+    }, []);
 
-const MaterialUILayout = ({children}) => (
+    return (
     <DateUtils>
         <MuiThemeProvider theme={theme}>
             {children}
         </MuiThemeProvider>
     </DateUtils>
-);
+)};
 
 MaterialUILayout.propTypes = {
     children: PropTypes.any
