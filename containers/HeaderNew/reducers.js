016a754944d62f48289d091bfd12426fca21bd55
babbageLabs <fgkinus@gmail.com>
2024-02-01 17:28:41 +0300

    fix reducer logic for header items #BPCA-2322:wq

--- containers/HeaderNew/reducers.js
+++ containers/HeaderNew/reducers.js
@@ -1,14 +1,24 @@
-import { List } from 'immutable';
+import { fromJS } from 'immutable';
 import * as actionTypes from './actionTypes';
 
-const defaultHeaderInfo = new List();
+const defaultHeaderInfo = fromJS([]);
 
 export function headerInfo(state = defaultHeaderInfo, action) {
     switch (action.type) {
         case actionTypes.SET_HEADER_INFO:
             if (Array.isArray(action.headerInfo)) {
-                const headerInfo = new List(action.headerInfo);
-                return state.mergeDeep(headerInfo);
+                if (!state.size) return fromJS(action.headerInfo);
+
+                const currentItems = state.toJS();
+                const newItems = action.headerInfo.map(item => item.label);
+                const mergedArray = currentItems.filter(item => !newItems.includes(item.label)).concat(action.headerInfo);
+                return fromJS(mergedArray);
+            }
+            break;
+        case actionTypes.REMOVE_HEADER_ITEM:
+            if (typeof action.label === 'string') {
+                const filteredList = state.filter(item => item.get('label') !== action.label);
+                return filteredList;
             }
             break;
         default:
