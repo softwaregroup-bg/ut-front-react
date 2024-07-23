cf085c66c75dd009ee238e904226b539c8f60b58
mshumankov <44842085+mshumankov@users.noreply.github.com>
2024-07-23 17:51:34 +0300

    fix: set date UTC when change startDate or endDate filter PS-289 (#696)
    * fix: set date UTC when change startDate or endDate filter PS-289
    
    * fix: remove report unnecessary time zone changes PS-289
    
    ---------
    
    Co-authored-by: Martin Shumankov <martin.shumankov@softwaregrouo.com>

--- components/GridToolBox/index.js
+++ components/GridToolBox/index.js
@@ -587,11 +587,6 @@ class GridToolBox extends Component {
                 }
             }
         });
-        // Set date to UTC
-        if (result.endDate && result.startDate && this.props.filterElements.find(el => el.utcTransform)) {
-            result.endDate = new Date(result.endDate.getTime() + result.endDate.getTimezoneOffset() * 60 * 1000);
-            result.startDate = new Date(result.startDate.getTime() + result.startDate.getTimezoneOffset() * 60 * 1000);
-        }
 
         if (filterIframe) {
             result = { ...result, ...filterIframe };
