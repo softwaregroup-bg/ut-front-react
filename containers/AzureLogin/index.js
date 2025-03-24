d3043505a4273f020ede96cd4e5c39040a82f684
ekchirchir <emmanuel.chirchir@softwaregroup.com>
2025-03-24 13:22:13 +0300

    STBA-1453 fix lint errors

--- containers/AzureLogin/index.js
+++ containers/AzureLogin/index.js
@@ -6,26 +6,27 @@ class AzureLogin extends Component {
         const url = `${window.location.origin}/rpc/login/oidc/initiate/azure`;
         window.location = url;
     }
+
     render() {
         return (
             <button className={style.azureLoginbButton} onClick={this.onButtonClick}>
                 <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 21 21"
-                className="microsoftLogo"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 21 21"
+                    className="microsoftLogo"
                 >
-                {/* Microsoft logo - four squares */}
-                <rect x="1" y="1" width="9" height="9" fill="#ffffff" />
-                <rect x="11" y="1" width="9" height="9" fill="#ffffff" />
-                <rect x="1" y="11" width="9" height="9" fill="#ffffff" />
-                <rect x="11" y="11" width="9" height="9" fill="#ffffff" />
+                    {/* Microsoft logo - four squares */}
+                    <rect x="1" y="1" width="9" height="9" fill="#ffffff" />
+                    <rect x="11" y="1" width="9" height="9" fill="#ffffff" />
+                    <rect x="1" y="11" width="9" height="9" fill="#ffffff" />
+                    <rect x="11" y="11" width="9" height="9" fill="#ffffff" />
                 </svg>
                 Sign in with Azure AD
             </button>
-        )
+        );
     }
 }
 
-export default AzureLogin
+export default AzureLogin;
